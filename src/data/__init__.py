4223aa8f275196202a586b18f9454a4c2d449957
JulianPMenon <97676461+JulianPMenon@users.noreply.github.com>
2026-01-16 17:56:07 +0100

    cleaned reopsitory

--- src/data/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Data module - Contains data loading and preprocessing utilities
-"""
-
-from .dataset import GenomicDataset
-from .dataloader import get_dataloaders
-
-__all__ = ["GenomicDataset", "get_dataloaders"]
--- src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utils module - Contains utility functions for logging, config, etc.
-"""
-
-from .logger import setup_logger
-from .config import load_config
-from .metrics import calculate_metrics
-
-__all__ = ["setup_logger", "load_config", "calculate_metrics"]
